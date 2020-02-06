04e17c56f9ed5c286eba2a1d3c814614d02f2e0b
dmytro.khl <dmytro.khludkov@vgs.io>
2020-02-06 21:09:53 +0200

    Fix headers

--- CardIOFramework/CardIOFramework.h
+++ CardIOFramework/CardIOFramework.h
@@ -15,13 +15,3 @@ FOUNDATION_EXPORT double CardIOFrameworkVersionNumber;
 FOUNDATION_EXPORT const unsigned char CardIOFrameworkVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <CardIOFramework/PublicHeader.h>
-
-
-#import <CardIO/CardIOCreditCardInfo.h>
-#import <CardIO/CardIODetectionMode.h>
-#import <CardIO/CardIOView.h>
-#import <CardIO/CardIOPaymentViewController.h>
-#import <CardIO/"CardIOViewDelegate.h>
-#import <CardIO/CardIOPaymentViewControllerDelegate.h>
-#import <CardIO/CardIOUtilities.h>
-
